64ffb53418649f83d811d0689d5585b1bca1cc8a
Kasper Kuznik <kasperr14@icloud.com>
2024-11-12 18:24:55 +0100

    Adding welcome message

--- Hangman.py
+++ Hangman.py
@@ -1,11 +1,4 @@
-# print("Welcome to hangman game\nProvide player names.")
-# player1 = input("Player 1: ")
-# print(f"Player 1 is {player1}\n")
-# player2 = input("Player 2: ")
-# print(f"Player 2 is {player2}\n")
-# print("Player one provides a word to guess")
-# word = input("Please provide a word to guess: ")
-# print(f"Word to guess: {len(word)*'_'}")
+print("Welcome to hangman game\n")
 
 def get_word():
     word = input("Please provide a word to guess: ")
